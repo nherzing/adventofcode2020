9a3d060f009a7493b59cc727ef5f390134cf075b
Nathan Herzing <nherzing@gmail.com>
2020-12-04 21:21:23 -0500

    Day 4

---
+++ d4/sol.lisp
@@ -0,0 +1,71 @@
+(defun read-lines (filename)
+  (with-open-file (stream filename)
+    (loop for line = (read-line stream nil)
+          while line
+          collect line)))
+
+(defparameter *lines* (read-lines "input.txt"))
+
+(defun split-at (item seq &key (test #'eql))
+  (let ((pos (position item seq :test test)))
+    (if pos
+        (cons (subseq seq 0 pos)
+              (split-at item (subseq seq (1+ pos)) :test test))
+        (list seq))))
+
+(defun join (strings sep)
+  (reduce #'(lambda (s acc) (concatenate 'string s sep acc)) (rest strings) :initial-value (first strings)))
+
+(defun passport-parse (str)
+  (mapcar
+       #'(lambda (str) (split-at ":" str :test #'string=))
+       (split-at " " str :test #'string=)))
+
+(defun passport-keys (passport)
+  (map 'list #'first passport))
+
+(defun passport-val (passport key)
+  (second (find key passport :key #'first :test #'string=)))
+
+(defconstant required-keys '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"))
+
+(defun validp (pp)
+  (let ((keys (passport-keys pp)))
+    (and (subsetp required-keys keys :test #'string=)
+         (let ((byr (parse-integer (passport-val pp "byr"))))
+           (and byr
+                (<= 1920 byr 2002)))
+         (let ((iyr (parse-integer (passport-val pp "iyr"))))
+           (and iyr
+                (<= 2010 iyr 2020)))
+         (let ((eyr (parse-integer (passport-val pp "eyr"))))
+           (and eyr
+                (<= eyr 2030)
+                (>= eyr 2020)))
+         (let* ((hgt (passport-val pp "hgt"))
+               (num (parse-integer (subseq hgt 0 (- (length hgt) 2))))
+               (unit (subseq hgt (- (length hgt) 2))))
+           (and num
+                (or (and
+                     (string= unit "cm")
+                     (<= 150 num 193))
+                    (and
+                     (string= unit "in")
+                     (<= 59 num 76)))))
+         (let ((hcl (passport-val pp "hcl")))
+           (and (char= (char hcl 0) #\#)
+                (= 7 (length hcl))
+                (every #'(lambda (c) (find c "0123456789abcdef" :test #'char=)) (subseq hcl 1))))
+         (find (passport-val pp "ecl") '("amb" "blu" "brn" "gry" "grn" "hzl" "oth") :test #'string=)
+         (let ((id (passport-val pp "pid")))
+           (and (= (length id) 9)
+                (parse-integer id)))
+         )))
+
+(defun solve ()
+  (let* ((raw-passports (map 'list #'(lambda (s) (join s " ")) (split-at "" *lines* :test #'string=)))
+        (passports (map 'list #'passport-parse raw-passports)))
+    (count-if #'validp passports)))
+
+
+(solve)
