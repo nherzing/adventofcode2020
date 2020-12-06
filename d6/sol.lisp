d61add59a09b3b8b6f3eff99692874f0156c818e
Nathan Herzing <nherzing@gmail.com>
2020-12-06 16:00:53 -0500

    Day 6

---
+++ d6/sol.lisp
@@ -0,0 +1,22 @@
+(defpackage :d5
+  (:use :common-lisp :common))
+
+(defparameter *lines* (read-lines "input.txt"))
+
+(defun count-group (group)
+  (length (reduce #'union (mapcar #'(lambda (person) (coerce person 'list)) group))))
+
+(defun solve ()
+  (let* ((groups (split-at "" *lines* :test #'string=)))
+    (apply #'+ (mapcar #'count-group groups))))
+
+(solve)
+
+(defun count-group2 (group)
+  (length (reduce #'intersection (mapcar #'(lambda (person) (coerce person 'list)) group))))
+
+(defun solve2 ()
+  (let* ((groups (split-at "" *lines* :test #'string=)))
+    (apply #'+ (mapcar #'count-group2 groups))))
+
+(solve2)
